d815d67baff73ad749c519344bb046e8f52fa2c1
robdockins <robdockins@fastmail.fm>
2005-12-18 12:51:29 -0800

    add haddock docs and copyright statement
    darcs-hash:20051218205129-f399b-ad4e6f3379eb02b921a06d404fa9bc62b30b50f8

--- src/System/Console/Regex.hs
+++ src/System/Console/Regex.hs
@@ -1,8 +1,27 @@
+
+{-
+ - 
+ -  Copyright 2005, Robert Dockins.
+ -  
+ -}
+
+{- | This module implements a pretty hokey version of regular expressions.
+ -   They are used to parse the arguments to shell commands and to give
+ -   information about the type of the argument at a position in the
+ -   string to allow positional word completion.  The REs are directly
+ -   intertreted in the list monad, so effeciency isn't so great, but
+ -   for most shell command lines that won't matter too much.
+ -}
+
 module System.Console.Regex where
 
 import Control.Monad
 import Numeric
 
+{- | The type of regular expressions.  Regular expressions evaluation
+     calculates a result value as well as recognizing strings in a language,
+     so we use a GADT which follows the structure of the evaluation.
+-}
 data Regex a x where
   Empty     :: Regex a x
   Epsilon   :: x -> Regex a x
@@ -14,6 +33,7 @@ data Regex a x where
   Alt       :: (Either p q -> x)       -> Regex a p -> Regex a q -> Regex a x
   Star      :: ([p] -> x)              -> Regex a p -> Regex a x
 
+{- | Auxiliary type used to help remove unnecessary parenthesis when printing REs -}
 data RegexContext
   = TopContext
   | AltContext
@@ -21,6 +41,10 @@ data RegexContext
   | StarContext
  deriving Eq
 
+{- | Print a string representation of a regular expression.
+     Really only useful for debugging becuase there is no
+     inverse (parser).
+-}
 showRegex :: RegexContext -> Regex a x -> String
 showRegex cxt (Label l _)      = l
 showRegex cxt Empty            = "%empty"
@@ -40,12 +64,24 @@ parenIf True str = "("++str++")"
 instance Show (Regex a x) where
   show = showRegex TopContext
 
+
+{- | Returns true if the regular expressions matches the 
+     input list -}
 matchesRegex :: Regex a x -> [a] -> Bool
 matchesRegex re ts = not (null (matchRegex re ts))
 
+{- | Generates a list of all calculated values from 
+     matches on the RE.  Returns the empty list if
+     there are no matches 
+-}
 matchRegex :: Regex a x -> [a] -> [x]
 matchRegex re ts = [ x | (x,[]) <- runRegex re ts ]
 
+{- | Generates a list corresponding to all
+     partial matches of the regular expression.
+     The first component is the calculated value and
+     the second component is the remaining unmatched input
+-}
 runRegex :: Regex a x -> [a] -> [(x,[a])]
 
 runRegex Empty ts = mzero
--- src/System/Console/Shell.hs
+++ src/System/Console/Shell.hs
@@ -1,4 +1,66 @@
-module System.Console.Shell where
+{-
+ - 
+ -  Copyright 2005, Robert Dockins.
+ -  
+ -}
+
+{- | This module implements a framework for creating read-eval-print style
+     command shells.  Shells are created by declarativly defining evaluation
+     functions and \"shell commands\".  Input is read using the standard Haskell
+     readline bindings, and the shell framework handles history and word completion
+     features.
+
+     The basic idea is:
+       1) Create a list of shell commands and an evaluation function
+       2) Create a shell description
+       3) Set up the initial shell state
+       4) Run the shell
+-}
+
+module System.Console.Shell (
+
+-- * Shell Descriptions
+  ShellDescription (..)
+, initialShellDescription
+, mkShellDescription
+
+-- * Executing Shells
+, runShell
+
+-- * Creating Shell Commands
+-- ** High-level Interface
+, exitCommand
+, helpCommand
+, cmd
+, CommandFunction (..)
+, FullCommand (..)
+, StateCommand (..)
+, SimpleCommand (..)
+, File (..)
+, Username (..)
+, Completable (..)
+, Completion (..)
+
+-- ** Low-Level Interface
+, ShellCommand
+
+-- * Subshells
+, Subshell
+, simpleSubshell
+, runSubshell
+
+-- * Printing Help Messages
+, showShellHelp
+, showCmdHelp
+
+-- * Type Synonyms and Auxiliary Types
+, CommandStyle (..)
+, CommandResult
+, EvaluationFunction
+, ShellSpecial (..)
+, CommandParseResult (..)
+, CommandParser
+) where
 
 import Maybe (isJust)
 import Data.Char (toLower, isDigit, isSpace)
@@ -15,36 +77,67 @@ import qualified System.Console.Readline as RL
 
 import System.Console.Regex
 
-data CommandStyle
-   = OnlyCommands
-   | ColonCommands
 
+-- | Datatype describing the style of shell commands.  This 
+--   determines how shell input is parsed.
+data CommandStyle
+   = OnlyCommands   -- ^ Indicates that all input is to be interpreted as shell commands; no
+                    --   input will be passed to the evaluation function.
+   | ColonCommands  -- ^ Indicates that commands are prefaced with a colon ':' character.
+
+-- | The type of results from shell commands.  They are either
+--   a \"special\" action for the shell framework to execute, or
+--   a modified shell state.
 type CommandResult st = Either ShellSpecial st
+
+-- | The type of an evaluation function for a shell.  The function
+--   takes the input string and the current shell state, and returns
+--   a possibly modified shell state.
 type EvaluationFunction st = String -> st -> IO st
 
+-- | Special commands for the shell framework.
 data ShellSpecial
-  = ShellExit
-  | ShellHelp (Maybe String)
+  = ShellExit                  -- ^ Causes the shell to exit
+  | ShellHelp (Maybe String)   -- ^ Causes the shell to print an informative message.
+                               --   If a command name is specified, only information about
+                               --   that command will be displayed.
 
+-- | The result of parsing a command.
 data CommandParseResult st
-  = CompleteParse (st -> IO (CommandResult st))
+  = CompleteParse (st -> IO (CommandResult st)) 
+          -- ^ A complete parse.  A command function is returned.
   | IncompleteParse (Maybe (st -> String -> IO [String]))
+          -- ^ An incomplete parse.  A word completion function may be returned.
 
+-- | The type of a command parser.
 type CommandParser st = String -> [CommandParseResult st]
+
+-- | The type of a shell command.  The shell description is passed in, and the
+--   tuple consists of
+--     (command name,command parser,command syntax,help message)
 type ShellCommand st = ShellDescription st -> (String,CommandParser st,String,String)
+
+-- | The type of subshells.  The tuple consists of
+--    1) A function to generate the initial subshell state from the outer shell state
+--    2) A function to generate the outer shell state from the final subshell state
+--    3) A function to generate the shell description from the inital subshell state
 type Subshell st st' = (st -> IO st', st' -> IO st, st' -> IO (ShellDescription st') )
 
+-- | A record type which describes the attributes of a shell.
 data ShellDescription st
    = ShDesc
-   { shellCommands      :: [ShellCommand st]
-   , commandStyle       :: CommandStyle
-   , evaluateFunc       :: EvaluationFunction st
-   , wordBreakChars     :: [Char]
-   , beforePrompt       :: st -> IO ()
-   , prompt             :: String
+   { shellCommands      :: [ShellCommand st]      -- ^ Commands for this shell
+   , commandStyle       :: CommandStyle           -- ^ The style of shell commands
+   , evaluateFunc       :: EvaluationFunction st  -- ^ The evaluation function for this shell
+   , wordBreakChars     :: [Char]                 -- ^ The characters upon which readline will break words
+   , beforePrompt       :: st -> IO ()            -- ^ an IO action to run before each prompt is printed
+   , prompt             :: String                 -- ^ The prompt to print
    , defaultCompletions :: Maybe (st -> String -> IO [String])
+                                                  -- ^ If set, this function provides completions when NOT
+                                                  --   in the context of a shell command
    }
 
+-- | A basic shell description with sane initial values
 initialShellDescription :: IO (ShellDescription st)
 initialShellDescription =
   do let wbc = " \t\n\r\v`~!@#$%^&*()=[]{};\\\'\",<>"
@@ -58,6 +151,8 @@ initialShellDescription =
        , defaultCompletions = Just (\_ _ -> return [])
        }
 
+-- | Creates a simple shell description from a list of shell commmands and
+--   an evalation function.
 mkShellDescription :: [ShellCommand st] -> EvaluationFunction st -> IO (ShellDescription st)
 mkShellDescription cmds func =
    do desc <- initialShellDescription
@@ -73,6 +168,9 @@ data InternalShellState st
      , cancelHandler    :: Handler
      }
 
+-- | Run a shell.  Given a shell description and an initial state
+--   this function runs the shell until it exits, and then returns
+--   the final state.
 runShell :: ShellDescription st -> st -> IO st
 runShell desc init = bracket setupShell exitShell (\iss -> shellLoop desc iss init)
 
@@ -194,9 +292,12 @@ shellLoop desc iss init = loop init
              Nothing  -> putStrLn "cancled..." >> loop st
              Just st' -> loop st'
 
+-- | Prints the help message for this shell, which lists all avaliable
+--   commands with their syntax and a short informative message about each.
 showShellHelp :: ShellDescription st -> String
 showShellHelp desc = concat [ concat [syn,"\t\t",msg,"\n"] | (_,_,syn,msg) <- getShellCommands desc ]
 
+-- | Print the help message for a particular shell command
 showCmdHelp :: ShellDescription st -> String -> String
 showCmdHelp desc cmd =
   case cmds of
@@ -205,22 +306,28 @@ showCmdHelp desc cmd =
 
  where cmds = filter (\ (n,_,_,_) -> n == cmd) (getShellCommands desc)
 
-exitCommand :: String -> ShellCommand st
+-- | Creates a shell command which will exit the shell.
+exitCommand :: String            -- ^ the name of the command
+            -> ShellCommand st
 exitCommand name desc = ( name
                         , \_ -> [CompleteParse (\_ -> return (Left ShellExit))]
                         , concat [maybeColon desc,name]
                         , "Exit the shell"
                         )
 
-helpCommand :: String -> ShellCommand st
+-- | Creates a command which will print the shell help message.
+helpCommand :: String           -- ^ the name of the command
+            -> ShellCommand st
 helpCommand name desc = ( name
                         , \_ -> [CompleteParse (\_ -> return (Left (ShellHelp Nothing)))]
                         , concat [maybeColon desc,name]
                         , "Display the shell command help"
                         )
 
-simpleSubshell :: (st -> IO st')
-               -> ShellDescription st'
+-- | Creates a simple subshell from a state mapping function 
+--   and a shell description.
+simpleSubshell :: (st -> IO st')       -- ^ A function to generate the initial subshell state from the outer shell state
+               -> ShellDescription st' -- ^ A shell description for the subshell
                -> IO (Subshell st st')
 simpleSubshell toSubSt desc = do
   ref <- newEmptyMVar
@@ -229,7 +336,10 @@ simpleSubshell toSubSt desc = do
   let mkDesc _        = return desc
   return (toSubSt',fromSubSt,mkDesc)
 
-runSubshell :: Subshell st st' -> st -> IO st
+-- | Execute a subshell.
+runSubshell :: Subshell st st' -- ^ the subshell to execute
+            -> st              -- ^ the current state
+            -> IO st           -- ^ the modified state
 runSubshell (toSubSt, fromSubSt, mkSubDesc) st = do
   subSt   <- toSubSt st
   subDesc <- mkSubDesc subSt
@@ -237,24 +347,75 @@ runSubshell (toSubSt, fromSubSt, mkSubDesc) st = do
   st'     <- fromSubSt subSt'
   return st'
 
+-- | A shell command which can return shell special commands as well as 
+--   modifying the shell state
 newtype FullCommand st   = FullCommand (st -> IO (CommandResult st))
+
+-- | A shell command which can modify the shell state.
 newtype StateCommand st  = StateCommand (st -> IO st)
+
+-- | A shell command which does not alter the shell state.
 newtype SimpleCommand st = SimpleCommand (IO ())
+
+-- | Represents a command argument which is a filename
 newtype File = File String
+
+-- | Represents a command argument which is a username
 newtype Username = Username String
+
+-- | Represents a command argument which is an arbitrary
+--   completable item.  The type argument determines the
+--   instance of 'Completion' which is used to create
+--   completions for this command argument.
 newtype Completable compl = Completable String
 
+-- | A typeclass representing user definable completion functions.
 class Completion compl st | compl -> st where
+  -- | Actually generates the list of possible completions, given the
+  --   current shell state and a string representing the beginning of the word.
   complete :: compl -> (st -> String -> IO [String])
+
+  -- | generates a label for the argument for use in the help displays.
   completableLabel :: compl -> String
 
-cmd :: CommandFunction f st => String -> f -> String -> ShellCommand st
+-- | Creates a user defined shell commmand.
+cmd :: CommandFunction f st 
+    => String           -- ^ the name of the command
+    -> f                -- ^ the command function.  See 'CommandFunction' for restrictions
+                        --   on the type of this function.
+    -> String           -- ^ the help string for this command
+    -> ShellCommand st
+
 cmd name f helpMsg desc =
       (name
       ,parseCommand (wordBreakChars desc) f
       ,concat [maybeColon desc,name,commandSyntax f]
       ,helpMsg
       )
+
+
+-- | This class is used in the 'cmd' function to automaticly generate
+--   the command parsers and command syntax strings for user defined
+--   commands.  The type of 'f' is restricted to have a restricted set of
+--   monomorphic arguments ('Bool', 'Int', 'Integer', 'Float', 'Double', 'String', 
+--   'File', 'Username', and 'Completable') and the head type must be one of the three
+--   types 'FullCommand', 'StateCommand', or 'SimpleCommand'.  For example:
+--
+-- @
+--   f :: Int -> File -> FullCommand MyShellState 
+--   g :: Double -> StateCommand MyOtherShellState
+--   h :: SimpleCommand SomeShellState
+-- @
+--
+--   are all legal types, whereas:
+--
+-- @
+--   bad1 :: a -> FullCommand (MyShellState a)
+--   bad2 :: [Int] -> SimpleCommand MyShellState
+--   bad3 :: Bool -> MyShellState
+-- @
+--
+--   are not.
 
 class CommandFunction f st | f -> st where
   parseCommand  :: String -> f -> CommandParser st
